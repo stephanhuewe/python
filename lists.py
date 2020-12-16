5eef872b33ff454b7bd9202b5bb1260ddbb64f36
stephanhuewe <sh@huewe.info>
2020-12-16 10:57:46 +0100

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,15 @@
+Months = ["Jan","Feb","Mar","April","May","June"]
+
+# for loop
+for m in Months:
+	print(m)
+		
+# for with range
+for x in range (0,6):
+    print(Months[x])
+
+# while
+i = 0
+while i < 6:
+  print(Months[x]) #oops
+  i += 1
