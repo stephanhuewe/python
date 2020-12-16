604b783007b03ad705cb183e2627d2839fc494c5
stephanhuewe <sh@huewe.info>
2020-12-16 11:33:59 +0100

    Create Else.py

---
+++ CommonMistakes/Else.py
@@ -0,0 +1,11 @@
+degrees = 0
+
+degrees = int(input("Please enter your value:"))
+
+if (degrees > 100):
+    print ("It's hot, please turn off!")
+elif (degrees > 0 and degrees < 100):
+    print("It's OK"!)
+else: # No else
+    print("I have no idea! Please check twice")
+print ("You can leave your house...")
