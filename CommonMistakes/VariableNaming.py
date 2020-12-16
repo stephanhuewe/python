12c1482e4e4ca443f60ac6482253b8001b3194ee
stephanhuewe <sh@huewe.info>
2020-12-16 11:23:40 +0100

    Update VariableNaming.py

--- CommonMistakes/VariableNaming.py
+++ CommonMistakes/VariableNaming.py
@@ -1,11 +1,15 @@
-print(9 / 2)
-#4.5
-
-print( 9 // 2)
-#4
-
-print(9 % 2)
-#1
-
-print(-2 * 4 + 3 ** 2)
-# (3 ** 2) -> (-2 * 4) -> Added
+def add(x,y):
+    result = x +y
+    return result
+    
+def main():
+    x = 1
+    y = 2
+    a = 3
+    b = 4
+    result = 3
+    
+    ergebnis = add(a,b)
+    print(ergebnis)
+    
+main()
