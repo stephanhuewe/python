607c080d47b9c5a1644d27dc2c0c3e015d08cb3b
stephanhuewe <sh@huewe.info>
2021-11-23 21:26:34 +0100

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,13 @@
+# taken from: https://www.pythontutorial.net/python-basics/python-recursive-functions/
+def count_down(start):
+    """ Count down from a number  """
+    print(start)
+
+    # call the count_down if the next
+    # number is greater than 0
+    next = start - 1
+    if next > 0:
+        count_down(next)
+
+
+count_down(3)
