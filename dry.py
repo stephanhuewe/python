5808d2319125c678ed1afb6fdddbd80a5cd2f306
stephanhuewe <sh@huewe.info>
2023-11-28 21:57:38 +0100

    Create dry.py

---
+++ dry.py
@@ -0,0 +1,17 @@
+print ("Name: Emma")
+print ("Age: 26")
+
+print ("Name: Peter")
+print ("Age: 32")
+
+print ("Name: Julia")
+print ("Age: 35")
+
+
+def display_student(name, age):
+    print("Name: " + str(name))
+    print("Age: " + str(age))
+
+display_student("Emma", 26)
+display_student("Peter", 32)
+display_student("Julia", 35)
