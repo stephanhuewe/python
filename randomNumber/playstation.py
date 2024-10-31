37a6651bd432a21e650393c6c40edc48c80ff203
stephanhuewe <sh@huewe.info>
2024-10-31 09:02:11 +0100

    Create playstation.py

---
+++ randomNumber/playstation.py
@@ -0,0 +1,16 @@
+# https://www.engadget.com/2010-12-29-hackers-obtain-ps3-private-cryptography-key-due-to-epic-programm.html
+# Playstation 3 Cryptgraphic Hack
+
+def getRandomNumber():
+    return 4
+
+
+def main():
+    print("Lets guess a random number")
+    randomNo = getRandomNumber()
+    print("Here is one: ")
+    print(str(randomNo))
+
+
+if __name__ == "__main__":
+    main()
